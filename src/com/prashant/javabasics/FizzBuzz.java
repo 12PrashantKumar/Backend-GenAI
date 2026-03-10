d89cd95e8dcafd51440fd15e399f8f5c6336bef2
Prashant Kumar <12prashant2005@gmail.com>
2026-03-10 21:54:19 +0530

    Using For Loop

---
+++ src/com/prashant/javabasics/FizzBuzz.java
@@ -0,0 +1,28 @@
+package com.prashant.javabasics;
+
+
+
+public class FizzBuzz {
+	public static void main(String args[]) {
+		
+//		Scanner sc =new Scanner(System.in);
+		for (int i = 1; i <=100; i++) {
+			if(i%3==0 && i%5==0) {
+				System.out.println("FizzBuzz");
+			}
+			else if ( i%5==0){
+				System.out.println("Buzz");
+				
+			}
+			else if (i%3==0) {
+				System.out.println("Fizz");
+			}
+			else {
+				System.out.println(i);
+			}
+		}
+		System.out.println();
+		
+	}
+
+}
